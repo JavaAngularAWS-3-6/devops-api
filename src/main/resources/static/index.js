eaab98d7af6d4df933aa9f2426136af8fc5ef7fd
elmcgill <ethan.mcgill2016@gmail.com>
2023-03-30 13:33:09 -0500

    edited the mistakes to make the s3 work

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,7 +1,7 @@
 let root = document.getElementById("root");
 
 async function setupPage(){
-    let req = await fetch('https://35.175.64.209:8000/characters/');
+    let req = await fetch('http://35.175.64.209:8000/characters/');
     let characters = await req.json();
 
     let charactersDiv = document.createElement("div");
@@ -11,9 +11,9 @@ async function setupPage(){
 
     characters.forEach(character => {
         let characterElement = document.createElement("p");
-        characterElement.textContent = character.nickname;
+        characterElement.textContent = character.nickName;
         characterElement.className = "character";
-        characterElement.id = character.nickname;
+        characterElement.id = character.nickName;
         characterElement.addEventListener('click', displayCharacter);
         charactersDiv.appendChild(characterElement);
     });
@@ -26,17 +26,18 @@ async function displayCharacter(e){
     let info = document.getElementsByClassName('character-information')[0];
     info.innerHTML = '';
     let nickname = e.target.id;
-    let req = await fetch(`https://35.175.64.209:8000/characters/${nickname}`);
+    let req = await fetch(`http://35.175.64.209:8000/characters/${nickname}`);
     let character = await req.json();
     console.log(character);
     let image = document.createElement('img');
-    image.setAttribute('src', character.imageUrl);
+    console.log(character.imageURL);
+    image.setAttribute('src', character.imageURL);
     info.appendChild(image);
     let name = document.createElement('h1');
     name.textContent = `${character.firstName} ${character.lastName}`
     info.appendChild(name);
     let nick = document.createElement('h2');
-    nick.textContent = `Goes by: ${character.nickname}`;
+    nick.textContent = `Goes by: ${character.nickName}`;
     info.appendChild(nick);
     let creature = document.createElement('h2');
     creature.textContent = `Is a: ${character.creature}`;
